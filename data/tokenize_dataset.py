d2a18c6d80848ebd52c776db60400de09a83c078
Louis Arge <louislva@gmail.com>
2021-08-07 15:22:46 -0400

    remove legacy code

--- data/tokenize_dataset.py
+++
@@ -1,9 +0,0 @@
-from transformers import GPT2Tokenizer
-from pickle import dump
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-full_file = open("./dataset/lil_wayne_genius_dataset.txt", "r").read()
-tokens = tokenizer.encode(full_file)
-
-dump(tokens, open("./dataset/lil_wayne_genius_dataset_tokenized.pkl", "wb"))
